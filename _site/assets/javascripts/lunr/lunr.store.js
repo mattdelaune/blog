f11dc9bb1b36eb78dd38cd9d6bbc8d5a22ca8c26
Mdelaun <matthew.delaune@gmail.com>
2024-08-06 20:47:08 -0500

    Updated repo

--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -1,9 +1,4 @@
 var store = [{
-        "title": "What is Machine Learning? does it matter?",
-        "excerpt":"Many may ask, what is machine learning (ML)? We have heard this buzzword very often in the media or by big-name tech companies and lately, it has been used interchangeably with the word “artificial intelligence” (AI). These days, it is ubiquitous to hear this: “…the cameras on X smartphone is...","categories": [],
-        "tags": ["machine learning","artificial intelligence"],
-        "url": "http://localhost:4000/blog/2018/12/16/what-is-machine-learning.html"
-      },{
         "title": "Welcome to The Data Science Experience: A Beginner’s Journey into Data Science",
         "excerpt":"Welcome to The Data Science Experience! This blog was born out of my passion for data science and my journey from a SaaS sales professional to an aspiring data scientist. As someone who started in a completely different field, I know firsthand how overwhelming it can be to dive into...","categories": ["data-science","introduction"],
         "tags": ["data science","beginner","introduction"],
